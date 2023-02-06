5074ca22e8438143b4e2ef8e0e195b6bc7899330
ywang <ywang@vewd.com>
2023-02-06 16:12:11 +0800

    1. Extract Advertisement, RCUAdvertisement to Advertise module

---
+++ Advertise.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+import dbus.service
+import bluetooth_constants
+
+class Advertisement(dbus.service.Object):
+
+    def __init__(self, bus, path, index, advertising_type):
+        self.path = path + str(index)
+        self.bus = bus
+        self.ad_type = advertising_type
+        self.service_uuids = None
+        self.manufacturer_data = None
+        self.solicit_uuids = None
+        self.service_data = None
+        self.local_name = None
+        self.include_tx_power = None
+        self.data = None
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def get_properties(self):
+        properties = dict()
+        properties["Type"] = self.ad_type
+        if self.service_uuids is not None:
+            properties["ServiceUUIDs"] = dbus.Array(
+                self.service_uuids, signature="s")
+        if self.solicit_uuids is not None:
+            properties["SolicitUUIDs"] = dbus.Array(
+                self.solicit_uuids, signature="s")
+        if self.manufacturer_data is not None:
+            properties["ManufacturerData"] = dbus.Dictionary(
+                self.manufacturer_data, signature="qv"
+            )
+        if self.service_data is not None:
+            properties["ServiceData"] = dbus.Dictionary(
+                self.service_data, signature="sv"
+            )
+        if self.local_name is not None:
+            properties["LocalName"] = dbus.String(self.local_name)
+        if self.include_tx_power is not None:
+            properties["IncludeTxPower"] = dbus.Boolean(self.include_tx_power)
+
+        if self.data is not None:
+            properties["Data"] = dbus.Dictionary(self.data, signature="yv")
+        return {bluetooth_constants.ADVERTISEMENT_INTERFACE: properties}
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def add_service_uuid(self, uuid):
+        if not self.service_uuids:
+            self.service_uuids = []
+        self.service_uuids.append(uuid)
+
+    def add_solicit_uuid(self, uuid):
+        if not self.solicit_uuids:
+            self.solicit_uuids = []
+        self.solicit_uuids.append(uuid)
+
+    def add_manufacturer_data(self, manuf_code, data):
+        if not self.manufacturer_data:
+            self.manufacturer_data = dbus.Dictionary({}, signature="qv")
+        self.manufacturer_data[manuf_code] = dbus.Array(data, signature="y")
+
+    def add_service_data(self, uuid, data):
+        if not self.service_data:
+            self.service_data = dbus.Dictionary({}, signature="sv")
+        self.service_data[uuid] = dbus.Array(data, signature="y")
+
+    def add_local_name(self, name):
+        if not self.local_name:
+            self.local_name = ""
+        self.local_name = dbus.String(name)
+
+    def add_data(self, ad_type, data):
+        if not self.data:
+            self.data = dbus.Dictionary({}, signature="yv")
+        self.data[ad_type] = dbus.Array(data, signature="y")
+
+    @dbus.service.method(bluetooth_constants.DBUS_PROPERTIES, in_signature="s", out_signature="a{sv}")
+    def GetAll(self, interface):
+        if interface != bluetooth_constants.ADVERTISEMENT_INTERFACE:
+            raise bluetooth_exceptions.InvalidArgsException()
+        return self.get_properties()[bluetooth_constants.ADVERTISEMENT_INTERFACE]
+
+    @dbus.service.method(bluetooth_constants.ADVERTISEMENT_INTERFACE, in_signature="", out_signature="")
+    def Release(self):
+        print("%s: Released!" % self.path)
+
+class RCUAdvertisement(Advertisement):
+    BASE_PATH = "/org/bluez/rcu/advertisement"
+    def __init__(self, bus, index):
+        Advertisement.__init__(self, bus, self.BASE_PATH, index, "peripheral")
+        self.add_service_uuid('1812')
+        self.add_service_uuid('1802')
+        self.add_manufacturer_data(
+            0xFFFF, [0x70, 0x74],
+        )
+        #self.add_service_uuid(RCUService.RCU_SVC_UUID)
+
+        self.add_local_name("My_RCU")
+        self.include_tx_power = True
